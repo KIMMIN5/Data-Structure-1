f54d7d8974c2183ccf85198e9d479f7ececbe460
KIMMIN5 <121488861+KIMMIN5@users.noreply.github.com>
2023-11-06 22:53:24 +0900

    Add files via upload

---
+++ Test5_Array/TEST5_1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 10
+
+int stack[MAX_SIZE];
+int top = -1;
+
+void push(int data){
+    if(top == MAX_SIZE - 1){
+        fprintf(stderr, "Stack Full!\n");
+        return;
+    }
+    else {
+        top++;
+        stack[top] = data;
+        printf("%d push\n", data);
+    }
+}
+
+int pop(){
+    if(top == -1){
+        fprintf(stderr, "Stack Empty!\n");
+        return -1;
+    }
+    int data = stack[top];
+    top--;
+    printf("%d pop\n", data);
+    return data;
+}
+
+int main(){
+    srand(time(NULL));
+    
+    for(int i=0; i<30; i++) {
+        int rand_num = rand() % 100 + 1;
+        printf("[%d]", i + 1) ;
+        if(rand_num % 2 == 0){
+            push(rand_num);
+        }
+        else{
+            pop();
+        }
+    }
+    return 0;
+}
